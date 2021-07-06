91669deb36eef846ba443ef2512dfdf8f1e85858
amoreel <anthony.moreel62@hotmail.fr>
2021-07-06 17:07:54 +0200

    test de déploiment sur netify

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Mon premier site en ligne
         </p>
       </main>
 
